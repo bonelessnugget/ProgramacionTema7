d5844f2a266c4c63a1327b6965b2395a2ecc3777
bonelessnugget <yszeedyt99@gmail.com>
2022-02-21 09:31:56 +0100

    Minaya

--- Ejercicio2/src/Principal.java
+++ Ejercicio2/src/Principal.java
@@ -9,49 +9,17 @@ public class Principal {
 		System.out.println("Introduce el número de Administrativos que quieres crear: ");
 		nA = teclado.nextInt();
 		Persona[] admin = new Administrativos[nA];
-		for (int i=0; i<admin.length; i++) {
-			System.out.println("Introduce el nombre del trabajador: ");
-			String nombre = teclado.next();
-			System.out.println("Introduce el salario del trabajador: ");
-			float salario = teclado.nextFloat();
-			System.out.println("Introduce la edad del trabajador: ");
-			int edad = teclado.nextInt();
-			System.out.println("Introduce los estudios academicos: ");
-			String estudiosAcademicos = teclado.next();
-			System.out.println("Introduce el número de ordenadores: ");
-			int numOrdenadores = teclado.nextInt();
-			admin[i] = new Administrativos(nombre, salario, edad, estudiosAcademicos, numOrdenadores);
-		}
+		datosAdmin(teclado, admin);
 		
 		System.out.println("Introduce el número de Maquinistas quie quieres crear: ");
 		nM = teclado.nextInt();
 		Persona[] maqui = new Maquinistas[nM];
-		for (int i = 0; i<maqui.length; i++) {
-			System.out.println("Introduce el nombre del trabajador: ");
-			String nombre = teclado.next();
-			System.out.println("Introduce el salario del trabajador: ");
-			float salario = teclado.nextFloat();
-			System.out.println("Introduce la edad del trabajador: ");
-			int edad = teclado.nextInt();
-			System.out.println("Introduce el número de la máquina asignado: ");
-			int numMaquinas = teclado.nextInt();
-			System.out.println("Introduce los años que lleva con la máquina: ");
-			int anoMaquinas = teclado.nextInt();
-			maqui[i] = new Maquinistas(nombre, salario, edad, numMaquinas, anoMaquinas);
-		}
+		datosMaquinista(teclado, maqui);
 		
 		System.out.println("Introduce el número de empleados que no sean Administrativos o Maquinistas: ");
 		nO = teclado.nextInt();
 		Persona[] persona = new Persona[nO];
-		for (int i = 0; i<persona.length; i++) {
-			System.out.println("Introduce el nombre del trabajador: ");
-			String nombre = teclado.next();
-			System.out.println("Introduce el salario del trabajador: ");
-			float salario = teclado.nextFloat();
-			System.out.println("Introduce la edad del trabajador: ");
-			int edad = teclado.nextInt();
-			persona[i] = new Persona(nombre, salario, edad);
-		}
+		datosEmpleadoNormal(teclado, persona);
 
 		for (int i = 0; i<admin.length; i++) {
 			admin[i].visualizarTodosDatos();
@@ -67,6 +35,50 @@ public class Principal {
 			persona[i].visualizarTodosDatos();
 		}
 		
+	}
+
+	public static void datosEmpleadoNormal(Scanner teclado, Persona[] persona) {
+		for (int i = 0; i<persona.length; i++) {
+			System.out.println("Introduce el nombre del trabajador: ");
+			String nombre = teclado.next();
+			System.out.println("Introduce el salario del trabajador: ");
+			float salario = teclado.nextFloat();
+			System.out.println("Introduce la edad del trabajador: ");
+			int edad = teclado.nextInt();
+			persona[i] = new Persona(nombre, salario, edad);
+		}
+	}
+
+	public static void datosMaquinista(Scanner teclado, Persona[] maqui) {
+		for (int i = 0; i<maqui.length; i++) {
+			System.out.println("Introduce el nombre del trabajador: ");
+			String nombre = teclado.next();
+			System.out.println("Introduce el salario del trabajador: ");
+			float salario = teclado.nextFloat();
+			System.out.println("Introduce la edad del trabajador: ");
+			int edad = teclado.nextInt();
+			System.out.println("Introduce el número de la máquina asignado: ");
+			int numMaquinas = teclado.nextInt();
+			System.out.println("Introduce los años que lleva con la máquina: ");
+			int anoMaquinas = teclado.nextInt();
+			maqui[i] = new Maquinistas(nombre, salario, edad, numMaquinas, anoMaquinas);
+		}
+	}
+
+	public static void datosAdmin(Scanner teclado, Persona[] admin) {
+		for (int i=0; i<admin.length; i++) {
+			System.out.println("Introduce el nombre del trabajador: ");
+			String nombre = teclado.next();
+			System.out.println("Introduce el salario del trabajador: ");
+			float salario = teclado.nextFloat();
+			System.out.println("Introduce la edad del trabajador: ");
+			int edad = teclado.nextInt();
+			System.out.println("Introduce los estudios academicos: ");
+			String estudiosAcademicos = teclado.next();
+			System.out.println("Introduce el número de ordenadores: ");
+			int numOrdenadores = teclado.nextInt();
+			admin[i] = new Administrativos(nombre, salario, edad, estudiosAcademicos, numOrdenadores);
+		}
 	}
 
 }
